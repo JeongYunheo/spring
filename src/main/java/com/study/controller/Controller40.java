47d8af6ab74e382dafb18a4393e7a361c9c8b111
jeongY <zoe1126@naver.com>
2024-05-16 10:14:44 +0900

    /api/some3의 get request handler 작성

--- src/main/java/com/study/controller/Controller40.java
+++ src/main/java/com/study/controller/Controller40.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,5 +28,14 @@ public class Controller40 {
     @PostMapping("/api/someurl2")
     public void method4() {
         System.out.println("Controller40.method4");
+    }
+
+    @GetMapping("/api/someurl3")
+    public void method5(@RequestParam("id") Integer id,
+                        @RequestParam("name") String name,
+                        @RequestParam("email") String email) {
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+        System.out.println("email = " + email);
     }
 }
