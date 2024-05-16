baad7f661538b8a4ae674064a00781c03fa656a3
jeongY <zoe1126@naver.com>
2024-05-16 15:08:50 +0900

    JavaBean 객체 응답

--- src/main/java/com/study/domain/MyBean412.java
+++ src/main/java/com/study/domain/MyBean412.java
@@ -1,8 +1,12 @@
 package com.study.domain;
 
+import lombok.Data;
+
+@Data
 public class MyBean412 {
     private String team;
     private MyBean411 info;
     private String[] item;
     private Boolean married;
+    private String name;
 }
