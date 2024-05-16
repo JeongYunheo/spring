8914d1f27f73c5a3dd528987c6501d7f4b02a4b2
jeongY <zoe1126@naver.com>
2024-05-16 14:29:47 +0900

    Map 객체 응답

--- src/main/java/com/study/controller/Controller42.java
+++ src/main/java/com/study/controller/Controller42.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/api/main42")
 public class Controller42 {
@@ -28,4 +31,16 @@ public class Controller42 {
         }
         return "보";
     }
+
+    @GetMapping("sub3")
+    @ResponseBody
+    public Map<String, Object> sub3() {
+        return Map.of("name", "흥민",
+                "age", 25,
+                "married", false,
+                "info", Map.of("team", "tottenham", "location", "런던"),
+                "item", List.of("축구공", "운동화", "유니폼"));
+
+    }
+
 }
