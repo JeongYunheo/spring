470618ba4cb54432f2e71ee676239e0bf7004c8c
jeongY <zoe1126@naver.com>
2024-05-20 16:23:44 +0900

    Controller + ResponseBody = RestController

--- src/main/java/com/study/controller/Controller45.java
+++ src/main/java/com/study/controller/Controller45.java
@@ -1,16 +1,14 @@
 package com.study.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 
-@Controller
+// @Controller
+// @ResponseBody   // 모든 메소드에 적용
+@RestController // Controller + ResponseBody
 @RequestMapping("/api/main45")
 public class Controller45 {
 
