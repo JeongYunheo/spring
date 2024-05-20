dee9188c8a45aefc7dd70f2f96c51206645c05f3
jeongY <zoe1126@naver.com>
2024-05-20 16:02:26 +0900

    file request parameter 받기
    MultipartFile 타입으로 받기

--- src/main/java/com/study/controller/Controller45.java
+++ src/main/java/com/study/controller/Controller45.java
@@ -6,16 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/api/main45")
 public class Controller45 {
 
     @PostMapping("sub1")
     public String sub1(@RequestParam("name") String name,
-                       @RequestParam("file") MultipartFile file) {
+                       @RequestParam("file") MultipartFile file) throws IOException {
         System.out.println("name = " + name);
         System.out.println("file = " + file.getOriginalFilename());
         System.out.println("file.getSize() = " + file.getSize());
+
+        String path = "C:/Windows/Temp/" + file.getOriginalFilename();
+        file.transferTo(new File(path));
+
         return null;
     }
 }
